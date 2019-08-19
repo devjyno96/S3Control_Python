3d66cd86854ab34ff84df1bc11febc8a20750ba9
JINHO <jinho9613@naver.com>
2019-08-19 16:08:09 +0900

    progress percent
    진행률 표시 추가

--- S3Control.py
+++ S3Control.py
@@ -2,28 +2,44 @@ import boto3
 from pprint import pprint
 import hashlib
 import time
-
-
-
+import os
 '''
 aws는 boto3 라이브러리를 통해 연결 할 수 있다.
 '''
-fileKey = '' #파일 키값 저장
+fileKey = ''
+# 파일 키값 저장
 
-fileName = '' #파일이름 파일의 위치까지 지정해도 상관업다. 스플릿 해서 잘라 사용할꺼니깐
+fileName = 'test.jpg'
+# 파일이름 파일의 위치까지 지정해도 상관업다. 스플릿 해서 잘라 사용할꺼니깐
 
-bucketName = 'awc20190723'  # 내 버킷 이름
+bucketName = 'awc20190723'
+# 내 버킷 이름
 
-s3 = boto3.resource('s3') #전체 함수에서 사용할 객체
+
+s3 = boto3.resource('s3')
+# 전체 함수에서 사용할 객체
+
+statinfo = os.stat(fileName)
 
 
-def getHash(string): #파일명 + 현재시간 을 sha1코드로 변환
+#진행도를 나타내기 위한 함수
+progressBytes = 0
 
-    #입력받은 문자열 + 현재 시간(형식 :  1510647686.5457149 ) 을 해쉬매핑
+def progressPercent(chunk): # 업로드한 byte 입력됨 누적시켜야함
+    global progressBytes
+    if progressBytes >= statinfo.st_size :
+        progressBytes = 0
+    progressBytes += chunk
+    print(progressBytes / statinfo.st_size * 100)
+
+
+def getHash(string): # 파일명 + 현재시간 을 sha1코드로 변환
+
+    # 입력받은 문자열 + 현재 시간(형식 :  1510647686.5457149 ) 을 해쉬매핑
     changeString = string.encode('utf-8') + str(time.time()).encode('utf-8')
     sha = hashlib.sha1(changeString)
 
-    #hexdigent 방법을 이용하여 객체로 부터 해시값을 16진법으로 구함
+    # hexdigent 방법을 이용하여 객체로 부터 해시값을 16진법으로 구함
     hexSha1 = sha.hexdigest()
     return hexSha1
 
@@ -41,7 +57,8 @@ def getMetadate(fileKeyGet) :
 def upload(filePathUp) :
     fileNameUp = filePathUp.split('/').pop()#파일 이름을 / 로 나눈 배열의 맨 끝
     fileKeyUp = getHash((fileNameUp))
-    s3.Bucket(bucketName).upload_file(filePathUp, fileKeyUp)
+    s3.Bucket(bucketName).upload_file(filePathUp, fileKeyUp, Callback = progressPercent)
+
     '''
     s3.Bucket(MyBucketName).upload_file(uploadFileInMyComputer, uploadFileNameInS3)
     '''
@@ -58,7 +75,7 @@ def upload(filePathUp) :
 def download(fileKeyDown) :
     metadata = getMetadate(fileKeyDown)
     fileNameDown = metadata['Metadata']['filename']
-    s3.Bucket(bucketName).download_file(fileKeyDown, fileNameDown)
+    s3.Bucket(bucketName).download_file(fileKeyDown, fileNameDown, Callback = progressPercent)
     #다운 받고싶은 바일 이름을 바꾸고 싶아면 fileNameDown 변경
     '''
     s3.Bucket(MyBucketName).download_file(target_Key_Name_In_S3, file_name_As_MyComputer)
@@ -84,3 +101,7 @@ def getList() :
         keys.append(obj['Key'])
     print(keys)
     #일단 key값만 가져오게 만들었다 다음에 쓸모에 따라 수정하자
+
+
+fileKey = upload(fileName)
+download(fileKey)
